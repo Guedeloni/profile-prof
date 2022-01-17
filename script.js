489659ea7e6c577aab7982fff51e9b66bc94b311
Guenole Quideau <gqprof@protonmail.com>
2022-01-17 12:10:29 +0100

    Modif torch effect

--- script.js
+++ script.js
@@ -1,22 +1,37 @@
 function move(event) {
-  let X = event.clientX || event.touches[0].clientX;
-  let Y = event.clientY || event.touches[0].clientY;
+  // let X = event.clientX || event.touches[0].clientX;
+  // let Y = event.clientY || event.touches[0].clientY;
+  let X = event.clientX;
+  let Y = event.clientY;
   document.documentElement.style.setProperty ('--X', X + 'px');
   document.documentElement.style.setProperty ('--Y', Y + 'px');
 }
 
-let blackout = document.getElementById('blackout');
-let light = document.getElementById('footer');
+let header = document.getElementById('header');
+let card1 = document.getElementById('card1');
+let card2 = document.getElementById('card2');
+let footer = document.getElementById('footer');
 
-blackout.addEventListener('mouseover', (event) => {
-  console.log("********blackout hover**********");
-  // console.log(document.documentElement.style.display);
+card2.addEventListener('mousemove', (event) => {
+  console.log("********card2 hover**********");
+  document.documentElement.style.setProperty ('--OPACITY', 1);
   document.documentElement.style.setProperty ('--DISPLAY', 'block');
   move(event);
 });
 
-light.addEventListener('mouseover', (event) => {
-  console.log("********blackout not hover**********");
+header.addEventListener('mouseover', (event) => {
+  console.log("********card2 not hover**********");
+  document.documentElement.style.setProperty ('--OPACITY', 0.1);
+  document.documentElement.style.setProperty ('--DISPLAY', 'none');
+});
+card1.addEventListener('mouseover', (event) => {
+  console.log("********card2 not hover**********");
+  document.documentElement.style.setProperty ('--OPACITY', 0.1);
+  document.documentElement.style.setProperty ('--DISPLAY', 'none');
+});
+footer.addEventListener('mouseover', (event) => {
+  console.log("********card2 not hover**********");
+  document.documentElement.style.setProperty ('--OPACITY', 0.1);
   document.documentElement.style.setProperty ('--DISPLAY', 'none');
 });
 
